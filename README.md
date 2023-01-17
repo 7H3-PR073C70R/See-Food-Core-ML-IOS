b3e6fd4acc35d6d6d66d6524f425ee922087154f
7H3-PR073C70R <okanlawondamilare24434@gmail.com>
2023-01-18 01:02:26 +0200

    done

---
+++ README.md
@@ -0,0 +1,1 @@
+# See-Food-Core-ML-IOS
--- See Food/ViewController.swift
+++ See Food/ViewController.swift
@@ -6,14 +6,71 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = true
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = userPickedImage
+            
+            guard let ciImage = CIImage(image: userPickedImage) else {
+                fatalError("Couldn't conver to CIImage")
+            }
+            detectImage(image: ciImage)
+        }
+        
+        
+    }
+    
+    func detectImage(image: CIImage) {
+        do {
+            let model = try VNCoreMLModel(for: Inceptionv3().model)
+            let request = VNCoreMLRequest(model: model) { request, error in
+                guard let result = request.results as? [VNClassificationObservation] else {
+                    fatalError("Model Failed to process image")
+                }
+                if let firstResult = result.first {
+                    self.navigationItem.title = firstResult.identifier
+                    var alert = UIAlertController()
+                    alert.title = firstResult.identifier
+                    alert.present(self, animated: true)
+                }
+            }
+            let handler = VNImageRequestHandler(ciImage: image)
+            
+            do {
+                try handler.perform([request])
+            } catch {
+                var alert = UIAlertController()
+                alert.title = "Error!!!"
+                alert.present(self, animated: true)
+            }
+        } catch {
+            var alert = UIAlertController()
+            alert.title = "Error!!!"
+            alert.present(self, animated: true)
+        }
     }
 
 
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true)
+    }
 }
 
